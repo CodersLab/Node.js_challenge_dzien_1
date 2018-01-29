24f2a835d656a1ccc9f57d4367ca8ba2705cc76b
Maciej Judka <maciej.judka@gmail.com>
2018-01-29 23:48:26 +0100

    Update zadanieDnia

--- app/zadanieDnia.js
+++ app/zadanieDnia.js
@@ -1,7 +1,5 @@
 let tab = process.argv.slice(2).map(x => Number(x));
 
-sleepSort = (tab) => {
-	tab.forEach(x => setTimeout(() => console.log(x), x*1000))
-}
+sleepSort = tab => tab.forEach(x => setTimeout(() => console.log(x), x*1000))
 
 sleepSort(tab);
