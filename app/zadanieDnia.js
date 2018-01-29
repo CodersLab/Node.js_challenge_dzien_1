67699bddb0bdf726ecdea3c98867faf11f108a32
Maciej Judka <maciej.judka@gmail.com>
2018-01-29 23:36:51 +0100

    Add zadanieDnia

--- app/zadanieDnia.js
+++ app/zadanieDnia.js
@@ -1,1 +1,10 @@
-//Twój kod
+let tab = [...process.argv.slice(2).map(x => Number(x))];
+
+oneSeries = (num) => setTimeout(() => console.log(num), num*num);
+
+sleepSort = (tab) => {
+	let fullTime = tab.reduce((x, y) => x + y) * 1000;
+	setTimeout(() => tab.forEach(x => oneSeries(x)), fullTime);
+}
+
+sleepSort(tab);
