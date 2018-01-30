26c5dbbcc701642d530e6e957ecb266acea019b0
RadekM86 <byrm.design@gmail.com>
2018-01-30 17:41:44 +0100

    Zautomatyzowane zadanie :)

--- app/zadanie02.js
+++ app/zadanie02.js
@@ -1,23 +1,10 @@
-//Twój kod
-console.log('Witam');
-let myTimeout1 = setTimeout(()=>{
-    console.log("się")
-},1000);
-let myTimeout2 = setTimeout(()=>{
-    console.log("z")
-},2000);
-let myTimeout3 = setTimeout(()=>{
-    console.log("programem")
-},3000);
-let myTimeout4 = setTimeout(()=>{
-    console.log("Node.js")
-},4000);
-let myTimeout5 = setTimeout(()=>{
-    console.log("w konsoli")
-},5000);
-let myTimeout6 = setTimeout(()=>{
-    console.log("i korzystam")
-},6000);
-let myTimeout7 = setTimeout(()=>{
-    console.log("z funkcji czasu!")
-},7000);
+// //Twój kod
+//wersja 2
+
+const arr = ["Witam", "się", "z", "programem", "Node.js", "w konsoli", "i korzystam", "z funkcji czasu"]
+
+for(let i=0; i<arr.length;i++){
+    const myTimeoutAutomated = setTimeout(()=>{
+        console.log(arr[i])
+    }, i*1000)
+}
