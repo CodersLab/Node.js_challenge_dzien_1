26dee95a8444ebc3cd2ae36bcadfc82eec14e0db
Filip Godziński <godzinski.filip@gmail.com>
2018-01-30 20:48:15 +0100

    Zadanie 02 - done

--- app/zadanie02.js
+++ app/zadanie02.js
@@ -1,1 +1,31 @@
 //Twój kod
+
+const text1 = setTimeout( () => {
+    console.log("Node.js");
+}, 4000);
+
+const text2 = setTimeout( () => {
+    console.log("się");
+}, 1000);
+
+console.log("Witam");
+
+const text3 = setTimeout( () => {
+    console.log("i korzystam");
+}, 6000);
+
+const text4 = setTimeout( () => {
+    console.log("w konsoli");
+}, 5000);
+
+const text5 = setTimeout( () => {
+    console.log("z funkcji czasu!");
+}, 7000);
+
+const text6 = setTimeout( () => {
+    console.log("z");
+}, 2000);
+
+const text7 = setTimeout( () => {
+    console.log("programem");
+}, 3000);
