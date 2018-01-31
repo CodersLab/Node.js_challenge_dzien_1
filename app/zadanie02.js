6c826da4d0324233de1586d32059f58eea3117df
Filip Godziński <godzinski.filip@gmail.com>
2018-01-31 10:20:45 +0100

    simpler solution

--- app/zadanie02.js
+++ app/zadanie02.js
@@ -1,31 +1,18 @@
 //Twój kod
 
-const text1 = setTimeout( () => {
-    console.log("Node.js");
-}, 4000);
+const arrText = {
+    4: "Node.js",
+    1: "się",
+    0: "Witam",
+    6: "i korzystam",
+    5: "w konsoli",
+    7: "z funkcji czasu!",
+    2: "z",
+    3: "programem"
+};
 
-const text2 = setTimeout( () => {
-    console.log("się");
-}, 1000);
-
-console.log("Witam");
-
-const text3 = setTimeout( () => {
-    console.log("i korzystam");
-}, 6000);
-
-const text4 = setTimeout( () => {
-    console.log("w konsoli");
-}, 5000);
-
-const text5 = setTimeout( () => {
-    console.log("z funkcji czasu!");
-}, 7000);
-
-const text6 = setTimeout( () => {
-    console.log("z");
-}, 2000);
-
-const text7 = setTimeout( () => {
-    console.log("programem");
-}, 3000);
+for (let i in arrText) {
+    setTimeout( () => {
+        console.log(arrText[i]);
+    }, i*1000);
+}
