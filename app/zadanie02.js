61c2539827d356918167a3b22596136306c52f9c
Markowiecki <markowiecki.z@gmail.com>
2018-01-31 13:30:44 +0100

    Uzupełnione zadania

--- app/zadanie02.js
+++ app/zadanie02.js
@@ -1,1 +1,15 @@
-//Twój kod
+const msgText = {
+    4: "Node.js",
+    1: "się",
+    0: "Witam",
+    6: "i korzystam",
+    5: "w konsoli",
+    7: "z funkcji czasu!",
+    2: "z",
+    3: "programem"
+};
+for (let i in msgText) {
+    setTimeout( () => {
+        console.log(msgText[i]);
+    }, i*1000);
+}
--- app/zadanieDnia.js
+++ app/zadanieDnia.js
@@ -1,1 +1,7 @@
-//Twój kod
+process.argv.forEach((val, index) => {
+  if(index>1){
+    setTimeout(() => {
+      console.log(val);
+    },parseInt(val) * 1000);
+ }
+});
