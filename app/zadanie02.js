ecb8bb1f1d3c3e0ed96a9e5e5923a88f9236b023
ArkadiuszDz <arkadiusz.dziechciarz@gmail.com>
2018-02-01 22:02:28 +0100

    Node.js dzien 1

--- app/zadanie02.js
+++ app/zadanie02.js
@@ -1,1 +1,13 @@
 //Twój kod
+
+console.log('Witam');
+
+let textArray = ['się','z','programem','Node.js','w konsoli','i korzystam','z funkcji czasu!'];
+let counter = 0;
+let myInterval = setInterval(() => {
+  console.log(`${textArray[counter]}`);
+  counter++;
+  if (counter == textArray.length-1){
+    clearInterval(myInterval);
+  }
+},1000);
--- app/zadanie03.js
+++ app/zadanie03.js
@@ -1,1 +1,6 @@
 //Twój kod
+
+let [element,index,...array] = [...process.argv];
+
+let result = array.reduce((prev,next) => parseInt(prev,10)+parseInt(next,10));
+console.log(result);
