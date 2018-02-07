adce8350aeac09415684deba1702b1b8360fd2cb
Anna Górzanowska <anna.gorzanowska@gmail.com>
2018-02-07 14:44:38 +0100

    Ex 03 done

--- app/zadanie03.js
+++ app/zadanie03.js
@@ -1,1 +1,8 @@
-//Twój kod
+function sumProcess() {
+    let sum = 0;
+    for (i = 2; i < process.argv.length; i++) {
+        sum += parseInt(process.argv[i]);
+    }
+    console.log(sum);
+}
+sumProcess();
