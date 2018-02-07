328e64a051ad76ea90c12c02172a016d69bc791c
Anna Górzanowska <anna.gorzanowska@gmail.com>
2018-02-07 14:10:03 +0100

    Ex.02 done

--- app/zadanie02.js
+++ app/zadanie02.js
@@ -1,1 +1,32 @@
-//Twój kod
+function message1 (){
+    console.log('Node.js');
+}
+function message2 (){
+    console.log('się');
+}
+function message3 (){
+    console.log('Witam');
+}
+function message4 (){
+    console.log('i korzystam');
+}
+function message5 (){
+    console.log('w konsoli');
+}
+function message6 (){
+    console.log('z funkcji czasu!');
+}
+function message7 (){
+        console.log('z');
+}
+function message8 (){
+    console.log('programem');
+}
+message3();
+setTimeout(message1, 4000);
+setTimeout(message2, 1000);
+setTimeout(message4, 6000);
+setTimeout(message5, 5000);
+setTimeout(message6, 7000);
+setTimeout(message7, 2000);
+setTimeout(message8, 3000);
