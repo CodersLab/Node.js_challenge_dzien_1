05a277e9635d8e43d6c8670360e6fac691d0be88
Wojciech Chodkowski <chodkows@gmail.com>
2018-02-15 13:09:53 +0100

    finish first day challenge

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -1,1 +1,7 @@
 //Twój kod
+const name = 'Wojtek Chodkowski';
+const message = 'wita się z Node.js!'
+console.log(name);
+setTimeout(()=> {
+  console.log(message);
+}, 5000);
--- app/zadanieDnia.js
+++ app/zadanieDnia.js
@@ -1,1 +1,8 @@
 //Twój kod
+process.argv.forEach(( val, index ) => {
+  if( index !== 0 && index !== 1 ) {
+    setTimeout( () => {
+      console.log( parseInt( val ));
+    }, val * 1000 );
+  };
+});
