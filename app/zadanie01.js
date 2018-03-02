cfa495950e42ac87b28e2e2a4bbf0d784840d23f
vinilll <dzisvolodymyr@gmail.com>
2018-03-02 14:52:21 +0100

    Day 1 done!

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -1,1 +1,7 @@
 //Twój kod
+
+console.log("Włodek");
+
+setTimeout(()=>{
+    console.log("Wita się z Node.js")
+},5000);
--- app/zadanie02.js
+++ app/zadanie02.js
@@ -1,1 +1,78 @@
-//Twój kod
+// //Twój kod
+//
+// setTimeout(()=>{
+//     console.log("Node.js")
+// },4000);
+//
+// setTimeout(()=>{
+//     console.log("się")
+// },1000);
+//
+// setTimeout(()=>{
+//     console.log("Witam")
+// },);
+//
+// setTimeout(()=>{
+//     console.log("i korzystam")
+// },6000);
+//
+// setTimeout(()=>{
+//     console.log("z funkcji czasu")
+// },7000);
+//
+// setTimeout(()=>{
+//     console.log("z")
+// },2000);
+//
+// setTimeout(()=>{
+//     console.log("programem")
+// },3000);
+//
+
+const sec = 1000;
+const	array = [
+    		{
+    			txt: 'Node.js',
+    			time: 4,
+    		},
+		{
+			txt: 'się',
+    			time: 1,
+    		},
+		{
+			txt: 'Witam',
+    			time: 0,
+    		},
+		{
+			txt: 'i korzystam',
+    			time: 6,
+    		},
+		{
+			txt: 'w konsoli',
+    			time: 5,
+    		},
+		{
+			txt: 'z funkcji czasu!',
+    			time: 7,
+    		},
+		{
+			txt: 'z',
+				time: 2,
+    		},
+		{
+				txt: 'programem',
+            time: 3,
+    		},
+	];
+
+
+
+array.forEach(el => {
+    pokaz_text(el)
+});
+
+function pokaz_text(obj){
+    setTimeout(()=>{
+        console.log(obj.txt)
+    },obj.time * sec);
+}
