6f6d9ecfcc487d5aca8fde26e8181409b197b334
kamilasmieko <ksmieko@gmail.com>
2018-04-13 22:14:10 +0200

    Completed node.js challenge Day1

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -1,1 +1,10 @@
-//Twój kod
+/**
+ 1. Rozgrzewka!
+ Napisz taki program Node.js, w którym wyświetlisz swoje imię i nazwisko. Następnie po 5 sekundach program
+ powinien jeszcze wypisać wita się z Node.js! i zakończyć swoje działanie.
+ */
+
+console.log('Kamila');
+setTimeout(function () {
+    console.log('wita się z Node.js!')
+},5000);
