bc272645204c7fb2487b87d459a564a00cd41106
Krzysztof Urbaniak <kurbaniak06@gmail.com>
2018-09-05 23:47:13 +0100

    zadanie01.js

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -1,1 +1,10 @@
-//Twój kod
+console.log('Krzysztof Urbaniak');
+let counter = 0;
+ const intervalId = setInterval(() => {
+     console.log('wita sie z Node.js!'[counter]);
+     counter++;
+
+     if (counter === 'wita sie z Node.js!'.length){
+         clearInterval(intervalId);
+ }
+}, 5000);
