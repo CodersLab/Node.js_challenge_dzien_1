5fa3ecd9109ada3dfb07b73af5a19684e2ab82e9
Aleksandra Pochec <aleksandra.pochec@hotmail.com>
2018-09-13 22:14:57 +0200

    zadanie 1 i 2

--- app/zadanie02.js
+++ app/zadanie02.js
@@ -1,1 +1,49 @@
 //Twój kod
+
+console.log("Witam");
+
+setTimeout(() => {
+
+    console.log("Node.js");
+
+}, 4000);
+
+setTimeout(() => {
+
+    console.log("się");
+
+
+}, 1000);
+
+setTimeout(() => {
+
+    console.log("i korzystam");
+
+}, 6000);
+
+setTimeout(() => {
+
+    console.log("w konsoli");
+
+}, 5000);
+
+setTimeout(() => {
+
+    console.log("z funkcji czasu!");
+
+}, 7000);
+
+setTimeout(() => {
+
+    console.log("z");
+
+}, 2000);
+
+setTimeout(() => {
+
+    console.log("programem");
+
+}, 3000);
+
+
+//Witam się z programem Node.js w konsoli i korzystam z funkcji czasu
