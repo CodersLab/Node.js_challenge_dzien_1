1e163219527017615b6870a6acd6a1e8ff0f3169
Your Name <you@example.com>
2019-02-12 16:07:02 +0100

    Daily task complete

--- app/zadanieDnia.js
+++ app/zadanieDnia.js
@@ -1,1 +1,17 @@
 //Twój kod
+
+const arr = process.argv;
+const newArr=[];
+
+for(let i=2; i<arr.length; i++){
+    newArr.push(Number(arr[i]));
+};
+
+
+// console.log(newArr);
+newArr.forEach(function(el){
+
+    setTimeout(() => {
+        console.log(el)
+    }, el*1000 ) 
+})
